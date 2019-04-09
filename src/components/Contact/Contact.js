b2b3df2931107ebd6c0579acbbb8794a23d771de
Lee Klopfers <leeklopfers@Lees-MBP.fritz.box>
2019-04-09 23:48:47 +0200

    form-handler fuer netlify hinzugefuegt

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => (
           {({ isSubmitting, errors }) => (
              
             <FormWrapper>
-              <Form>
+              <Form method="POST" data-netlify="true">
                 <div>
                   
                   <p className="title">
